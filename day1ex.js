866dae5de2b50b43ef23875e3574d3aee853a6c4
argemiroanjos <argemirodosanjos17@gmail.com>
2026-02-25 19:49:31 -0300

    feat(logica): implementa soma de números pares com loop for

--- day1ex.js
+++ day1ex.js
@@ -1,6 +1,26 @@
+// Exercícios de lógica bruta
+
+//Total
 let total = 0
 
 for (let i = 1; i <= 5; i++) {
   total = total + i
   console.log("i:", i, "total:", total)
 }
+console.log("------------------------")
+// Soma de pares
+const numbers = [10,15,20,33,40];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  const num = numbers[i];
+  if (num % 2 === 0) {
+    soma += num;
+  }
+}
+console.log("Soma dos pares é:", soma);
+
+console.log("------------------------")
+
+// Busca da maior palavra
+const frase = "flutter é muito legal";
